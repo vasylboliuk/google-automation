c31ea130865c51ae4e695cca934a72c0b784a897
vasylboliuk <vbboliuk@gmail.com>
2016-01-26 23:07:27 +0200

    Add additional properties to Properties class

--- src/test/java/com/google/commons/Properties.java
+++ src/test/java/com/google/commons/Properties.java
@@ -2,6 +2,9 @@ package com.google.commons;
 
 public class Properties {
 	
-	public static final String APP_SERVER_URL = "https://www.google.com.ua/";
+	public static final String SERVER_URL = "https://www.google.com/";
+	public static final String URL_TEST_GTRANSLATE = "https://www.google.co.uk/";
 	public static final long WAITERS_TIMEOUT = 20;
+	public static final String PATH_TO_FIREFOX_34 = "c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"; //Firefox 34.0.5
+	public static final String PATH_TO_FIREFOX_43 = "c:\\Program Files\\Mozilla Firefox\\firefox.exe"; //Firefox 43.0.4
 }
