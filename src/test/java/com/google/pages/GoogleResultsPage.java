3b1a63a010cdad4bcdd56f31632974926a39cc25
vasylboliuk <vbboliuk@gmail.com>
2016-01-26 23:12:01 +0200

    Add GoogleResultsPage contents

--- src/test/java/com/google/pages/GoogleResultsPage.java
+++ src/test/java/com/google/pages/GoogleResultsPage.java
@@ -1,5 +1,28 @@
 package com.google.pages;
 
-public class GoogleResultsPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
+public class GoogleResultsPage {
+	protected WebDriver driver;
+	
+	
+	
+	public GoogleResultsPage(WebDriver driver) {
+		this.driver = driver;
+		//
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Click on result link in page
+	public void clickOnResultLinkInPage(){
+		driver.findElement(By.xpath("//div[@class='g'][1]//ancestor::h3//a")).click();
+	}
+	
+	public GoogleTranslatePage navigateToGoogleTranslate() throws Exception {
+		clickOnResultLinkInPage();
+		return new GoogleTranslatePage(driver);
+	}
+	
 }
