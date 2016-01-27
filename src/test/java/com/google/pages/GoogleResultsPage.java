57afbeb9970e6ea99afa987f8fc18fed34c81e91
vasylboliuk <vbboliuk@gmail.com>
2016-01-27 14:12:49 +0200

    Add new methods to GoogleResultsPage

--- src/test/java/com/google/pages/GoogleResultsPage.java
+++ src/test/java/com/google/pages/GoogleResultsPage.java
@@ -2,12 +2,20 @@ package com.google.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleResultsPage {
 	protected WebDriver driver;
 	
+		
+	@FindBy(xpath="//div[@class='rc']")
+	WebElement googleAllPageResults;
 	
+		
 	
 	public GoogleResultsPage(WebDriver driver) {
 		this.driver = driver;
@@ -23,6 +31,18 @@ public class GoogleResultsPage {
 	public GoogleTranslatePage navigateToGoogleTranslate() throws Exception {
 		clickOnResultLinkInPage();
 		return new GoogleTranslatePage(driver);
+	}
+	
+	//Navigation to result page
+	public GoogleResultsPage navigateToGoogleResultPage(int pageNumber) throws Exception{
+		driver.findElement(By.xpath("//a[@class='fl' and contains(text(),'"+pageNumber+"')]")).click();
+		new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[@class='cur' and contains(text(),'"+pageNumber+"')]")));
+		return new GoogleResultsPage(driver);
+		
+	}
+	//Get number of results in page
+	public int getNumberOfResults(){
+		return driver.findElements(By.xpath("//div[@class='rc']")).size();
 	}
 	
 }
