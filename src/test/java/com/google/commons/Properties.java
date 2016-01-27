874d8ccb0910c8a6d5320290f303b7600e5c4f97
vasylboliuk <vbboliuk@gmail.com>
2016-01-27 14:11:31 +0200

    Add small changes to Properties file

--- src/test/java/com/google/commons/Properties.java
+++ src/test/java/com/google/commons/Properties.java
@@ -2,8 +2,8 @@ package com.google.commons;
 
 public class Properties {
 	
-	public static final String SERVER_URL = "https://www.google.com/";
-	public static final String URL_TEST_GTRANSLATE = "https://www.google.co.uk/";
+	public static final String GOOGLE_PAGE_URL = "https://www.google.com/";
+	public static final String ENG_GOOGLE_PAGE_URL = "https://www.google.co.uk/";
 	public static final long WAITERS_TIMEOUT = 20;
 	public static final String PATH_TO_FIREFOX_34 = "c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"; //Firefox 34.0.5
 	public static final String PATH_TO_FIREFOX_43 = "c:\\Program Files\\Mozilla Firefox\\firefox.exe"; //Firefox 43.0.4
