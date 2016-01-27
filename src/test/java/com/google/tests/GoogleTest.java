42d0c499de4a872f38570d8860eca61fe052c71f
vasylboliuk <vbboliuk@gmail.com>
2016-01-27 14:30:31 +0200

    Divide two tests in separate class.

--- src/test/java/com/google/tests/GoogleTest.java
+++ src/test/java/com/google/tests/GoogleTest.java
@@ -10,7 +10,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import com.google.commons.Properties;
 import com.google.pages.GoogleResultsPage;
@@ -64,33 +63,5 @@ public class GoogleTest {
 	}
 	
 		
-	@Test
-	public void googleNavigationTest() throws Exception{
-		System.out.println("Test navigation.");
 		
-		GoogleSearchPage search = new GoogleSearchPage(driver);
-		search.searchDataInGoogle("Apple");
-		
-		GoogleResultsPage result = new GoogleResultsPage(driver);
-		int countResultsPage1 = result.getNumberOfResults();
-		System.out.println(countResultsPage1);
-			
-		result = result.navigateToGoogleResultPage(2);
-		int countResultsPage2 = result.getNumberOfResults();
-		System.out.println(countResultsPage2);
-		
-		result = result.navigateToGoogleResultPage(10);
-		int countResultsPage10 = result.getNumberOfResults();
-		System.out.println(countResultsPage10);
-		
-		//Verification: Results of Page 2 and 10
-		new SoftAssert().assertTrue(countResultsPage2==countResultsPage10);
-		
-		//Verification: Results of Page 1 and 10
-		new SoftAssert().assertTrue(countResultsPage1==countResultsPage10);
-		
-		//Verification: Results of Page 1 and 2
-		new SoftAssert().assertTrue(countResultsPage1==countResultsPage2);
-	}
-	
 }
