5129fe9915376edad758a28d0579040613cddfed
vasylboliuk <vbboliuk@gmail.com>
2016-02-07 12:52:37 +0200

    Change TEstProperties file

--- src/main/java/com/google/commons/TestProperties.java
+++ src/main/java/com/google/commons/TestProperties.java
@@ -8,26 +8,29 @@ import java.util.Properties;
 
 public class TestProperties {
 	
-	static final Properties props = new Properties();
-	static {
-		InputStream is = TestProperties.class.getResourceAsStream("config/config.properties");
-		try {
-			props.load(is);
-			is.close();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+	static Properties props;
+	static FileInputStream in;
+	
+	public static void readProperties() {
+		props = new Properties();
 		
+	try{
+		in = new FileInputStream("src/main/resources/config/config.properties");
+		props.load(in);
+		in.close();
+	}catch(Exception e){
+		e.printStackTrace();	
+		}
 	}
 	
-	public static final String GOOGLE_PAGE_URL = props.getProperty("GOOGLE_PAGE_URL");
-	public static final String ENG_GOOGLE_PAGE_URL = props.getProperty("ENG_GOOGLE_PAGE_URL");
-	public static final long WAITERS_TIMEOUT = Long.valueOf(props.getProperty("WAITERS_TIMEOUT"));
-	public static final String PATH_TO_FIREFOX_34 = props.getProperty("c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"); //Firefox 34.0.5
-	public static final String PATH_TO_FIREFOX_43 = props.getProperty("c:\\Program Files\\Mozilla Firefox\\firefox.exe"); //Firefox 43.0.4
+	public static String getGooglePageUrl() {
+		readProperties();
+		return props.getProperty("ENG_GOOGLE_PAGE_URL");
+	}
 	
-	
-//	public static String getProp(String propName) {
-//		return props.getProperty(propName);
-//	}
+	public static int getWaiterTimeout() {
+		readProperties();
+		return Integer.valueOf(props.getProperty("WAITERS_TIMEOUT"));
+	}
+		
 }
