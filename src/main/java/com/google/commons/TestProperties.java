a1b4d4a0ab3775792c1a7f932aa1b3103aba081d
vasylboliuk <vbboliuk@gmail.com>
2016-02-10 19:51:04 +0200

    Add fix for defect from Roman Svystun to TestProperties file.

--- src/main/java/com/google/commons/TestProperties.java
+++ src/main/java/com/google/commons/TestProperties.java
@@ -1,34 +1,42 @@
 package com.google.commons;
 
-
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class TestProperties {
-	
+
 	static Properties props;
 	static FileInputStream in;
-	
-	public static void readProperties() {
+
+	private static void readProperties() {
+		if (props != null) {
+			return;
+		}
+
 		props = new Properties();
-		
-	try{
-		in = new FileInputStream("src/main/resources/config/config.properties");
-		props.load(in);
-		in.close();
-	}catch(Exception e){
-		e.printStackTrace();	
+
+		try {
+			InputStream myIs = TestProperties.class.getResourceAsStream("/config/config.properties");
+			props.load(myIs);
+			myIs.close();
+			// in = new
+			// FileInputStream("src/main/resources/config/config.properties");
+			// props.load(in);
+			// in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	public static String getGooglePageUrl() {
 		readProperties();
 		return props.getProperty("ENG_GOOGLE_PAGE_URL");
 	}
-	
+
 	public static int getWaiterTimeout() {
 		readProperties();
 		return Integer.valueOf(props.getProperty("WAITERS_TIMEOUT"));
 	}
-		
+
 }
